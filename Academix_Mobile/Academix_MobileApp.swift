65e39cb891c95565482dd91122168262cf0dae85
mgKhantt <khantphonenaing156@gmail.com>
2025-08-29 23:26:57 +0630

    Initial Commit

---
+++ Academix_Mobile/Academix_MobileApp.swift
@@ -0,0 +1,17 @@
+//
+//  Academix_MobileApp.swift
+//  Academix_Mobile
+//
+//  Created by Khant Phone Naing  on 29/08/2025.
+//
+
+import SwiftUI
+
+@main
+struct Academix_MobileApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Academix_MobileTests/Academix_MobileTests.swift
@@ -0,0 +1,17 @@
+//
+//  Academix_MobileTests.swift
+//  Academix_MobileTests
+//
+//  Created by Khant Phone Naing  on 29/08/2025.
+//
+
+import Testing
+@testable import Academix_Mobile
+
+struct Academix_MobileTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
