bcf52fccc243c900a4999824979365982b3881bb
mgKhantt <khantphonenaing156@gmail.com>
2025-08-30 02:25:28 +0630

    first commit

---
+++ Academix_Mobile/Core/Components/CircleView.swift
@@ -0,0 +1,18 @@
+//
+//  CircleView.swift
+//  Academix_Mobile
+//
+//  Created by Khant Phone Naing  on 30/08/2025.
+//
+
+import SwiftUI
+
+struct CircleView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CircleView()
+}
---
+++ Academix_Mobile/Extensions/Colors.swift
@@ -0,0 +1,8 @@
+//
+//  Colors.swift
+//  Academix_Mobile
+//
+//  Created by Khant Phone Naing  on 30/08/2025.
+//
+
+import Foundation
